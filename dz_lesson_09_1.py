b026fb6eba021412d002fdc95797a370162480dc
Olesia Panchenko <olesya.aht@gmail.com>
2023-05-24 22:35:58 +0300

    Commit_02_20230524_2235

--- dz_lesson_09_1.py
+++ dz_lesson_09_1.py
@@ -1,14 +1,23 @@
-class My_Car:
+class Simple_Car(object):
     def __init__(self, mark, color, v_dvyguna):
         self.mark = mark
         self.color = color
         self.v_dvyguna = v_dvyguna
-def vpered(self):
-    return f"Машина {self.mark} їде вперед"
-def nazad(self):
-    return f"Машина {self.mark} їде назад"
+    def ahead(self):
+        return f"Car {self.mark} goes ahead"
+    def back(self):
+        return f"Car {self.mark} goes back"
 
-my_own_car = My_Car('Toyota','red','2')
-print(my_own_car)
-husband_car = My_Car('Volskvagen','black','2.4')
-print(husband_car)
+class Difficult_Car(Simple_Car):
+    def turn_left(self):
+        return f"Car {self.mark} turns left"
+    def turn_right(self):
+        return f"Car {self.mark} turns right"
+
+my_own_car = Simple_Car('Toyota','red','2')
+print('My car is',my_own_car.color)
+print(my_own_car.ahead())
+
+husband_car = Difficult_Car('Volskvagen','black','2.4')
+print('My husband car is',husband_car.color)
+print(husband_car.turn_right())
