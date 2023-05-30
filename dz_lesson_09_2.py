dc968fbb07d6d4509a65f370fcd4c93bc6f4f536
Olesia Panchenko <olesya.aht@gmail.com>
2023-05-30 14:23:33 +0300

    Commit_03_test_2_finish_20230530_1117

--- dz_lesson_09_2.py
+++ dz_lesson_09_2.py
@@ -1,19 +1,41 @@
 import string
 class TextProcessor:
+    # змінні: передається строка my_str, znak_punkt - змінна в яку будуть складатись усі знаки пунктуаціі
     def __init__(self, my_str):
         self.my_str = my_str
-        self.__znak_punkt = ""
+        self.znak_punkt = ""
     def get_clean_string(self):
         for i in self.my_str:
             if i in string.punctuation:
-                self.__znak_punkt = self.__znak_punkt + i
+                self.znak_punkt = self.znak_punkt + i
                 self.my_str = self.my_str.replace(i,'')
-        print(self.my_str)
-    def in_puktiation(self):
-        if len(self.__znak_punkt) != 0:
+        return self.my_str
+
+    def __in_puktiation(self):
+        if len(self.znak_punkt) != 0:
             return True
         else:
             return False
+    # Метод нижче для виводу результату попреднього прихованого методу
+    def get_print(self):
+        return self.__in_puktiation()
+class TextLoader(TextProcessor):
+    def __init__(self,text_processor):
+        self.__text_processor = TextProcessor(text_processor)
+        self.__clean_string = ""
+    @property
+    def set_clean_string(self):
+        self.__clean_string = self.__text_processor.get_clean_string()
+        print(f"Очищена строка: {self.__clean_string}")
 
+# Для перешої та другоі частини завдання:
 my_test_str = TextProcessor('vd%#nshku3+- gsfd/qao')
-my_test_str.get_clean_string()
+
+# Закоментований рядок нижче для перевірки False, якщо у рядку немає символів
+#my_test_str = TextProcessor('vdnshku3 gsfdqao')
+print(my_test_str.get_clean_string())
+print(my_test_str.get_print())
+
+# Для третьої частини завдання:
+my_test_str_2 = TextLoader('woru&^hvsud) hf')
+my_test_str_2.set_clean_string
